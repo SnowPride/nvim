ba795b147ac0ebf4019d8f407d69ffa292639d48
Manuel Carrer <manuelca@kjemi.uio.no>
2023-12-04 19:36:48 +0100

    Neotree improvements

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,6 +9,20 @@ return {
   },
   opts = {
     close_if_last_window = true,
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
+    },
+    buffers = {
+      bind_to_cwd = false,
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
+    },
   },
   keys = {
     {
