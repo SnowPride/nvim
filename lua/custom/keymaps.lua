f64aa7cb55a060d1b0d9a2055c24580bbe0a7d53
Manuel Carrer <manuelca@kjemi.uio.no>
2024-01-30 10:46:58 +0100

    Fix NotebookNavigator keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -3,21 +3,21 @@ require("which-key").register({
 })
 
 --Molten
-vim.keymap.set(
-  "n",
-  "<leader>jp",
-  "<cmd>MoltenInit python3<cr>",
-  { silent = true, noremap = true, desc = "Start Python 3 kernel" }
-)
-vim.keymap.set(
-  "n",
-  "<leader>jr",
-  "<cmd>MoltenReevaluateCell<cr>",
-  { silent = true, noremap = true, desc = "Re-evalute cell" }
-)
-vim.keymap.set(
-  "v",
-  "<leader>jc",
-  ":<c-u>MoltenEvaluateVisual<cr>gv",
-  { silent = true, noremap = true, desc = "Evaluate visual selection" }
-)
+-- vim.keymap.set(
+--   "n",
+--   "<leader>jp",
+--   "<cmd>MoltenInit python3<cr>",
+--   { silent = true, noremap = true, desc = "Start Python 3 kernel" }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<leader>jr",
+--   "<cmd>MoltenReevaluateCell<cr>",
+--   { silent = true, noremap = true, desc = "Re-evalute cell" }
+-- )
+-- vim.keymap.set(
+--   "v",
+--   "<leader>jc",
+--   ":<c-u>MoltenEvaluateVisual<cr>gv",
+--   { silent = true, noremap = true, desc = "Evaluate visual selection" }
+-- )
