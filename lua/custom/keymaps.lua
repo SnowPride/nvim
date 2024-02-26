03d64d5d2b277cf9979fb6b794f541293b003d4c
Manuel Carrer <manuelca@kjemi.uio.no>
2024-02-26 17:50:03 +0100

    Add todo-comments and trouble

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,3 +1,15 @@
 require("which-key").register({
   ["<leader>j"] = { name = "Jupyter", _ = "which_key_ignore", mode = { "n", "v" } },
 })
+
+require("which-key").register({
+  ["<leader>t"] = { name = "Todo", _ = "which_key_ignore", mode = { "n", "v" } },
+})
+vim.keymap.set("n", "<leader>tq", "<cmd>TodoQuickFix<cr>", { desc = "Open quickfix list with all the TODOs" })
+vim.keymap.set(
+  "n",
+  "<leader>td",
+  "<cmd>TroubleToggle document_diagnostics<cr>",
+  { desc = "Toggle Trouble for the current file" }
+)
+vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle todo<cr>", { desc = "Toggle Trouble for the current file" })
--- lua/custom/plugins/small_utils.lua
+++ lua/custom/plugins/small_utils.lua
@@ -35,4 +35,24 @@ return {
       },
     },
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      highlight = {
+        keyword = "fg",
+        pattern = [[(KEYWORDS)\s*(\([^\)]*\))?:]],
+      },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      auto_preview = false,
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
