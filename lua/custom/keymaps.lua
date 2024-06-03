5699837279e77b33066ae7a781c252c5e38634db
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-06-03 09:00:39 +0200

    Update keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,24 +1,24 @@
 require("which-key").register({
-  ["<leader>j"] = { name = "Jupyter", _ = "which_key_ignore", mode = { "n", "v" } },
+	["<leader>j"] = { name = "Jupyter", _ = "which_key_ignore", mode = { "n", "v" } },
 })
 
 require("which-key").register({
-  ["<leader>t"] = { name = "Todo", _ = "which_key_ignore", mode = { "n", "v" } },
+	["<leader>t"] = { name = "Trouble", _ = "which_key_ignore", mode = { "n", "v" } },
 })
 vim.keymap.set("n", "<leader>tq", "<cmd>TodoQuickFix<cr>", { desc = "Open quickfix list with all the TODOs" })
 vim.keymap.set(
-  "n",
-  "<leader>td",
-  "<cmd>TroubleToggle document_diagnostics<cr>",
-  { desc = "Toggle Trouble for the current file" }
+	"n",
+	"<leader>td",
+	"<cmd>TroubleToggle document_diagnostics<cr>",
+	{ desc = "Toggle Trouble for the current file" }
 )
-vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle todo<cr>", { desc = "Toggle Trouble for the current file" })
+vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle todo<cr>", { desc = "Toggle TODOs for the current file" })
 
 function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
