1202aacc7e0f77051a9722081dbae4e2686570e1
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-06-04 08:41:29 +0200

    Update neotree to always open root dir

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -1,34 +1,41 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  opts = {
-    close_if_last_window = true,
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = false,
-      },
-    },
-    buffers = {
-      bind_to_cwd = false,
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = false,
-      },
-    },
-  },
-  keys = {
-    {
-      "<leader>e",
-      "<cmd>Neotree toggle<cr>",
-      desc = "Toggle file explorer",
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	opts = {
+		close_if_last_window = true,
+		filesystem = {
+			bind_to_cwd = true,
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = false,
+			},
+		},
+		buffers = {
+			bind_to_cwd = false,
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = false,
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>e",
+			-- "<cmd>Neotree toggle<cr>",
+
+			function()
+				local root_patterns = { ".git" }
+				local root_dir = require("lspconfig").util.root_pattern(unpack(root_patterns))(vim.fn.expand("%:p"))
+				require("neo-tree.command").execute({ toggle = true, dir = root_dir })
+			end,
+
+			desc = "Toggle file explorer",
+		},
+	},
 }
