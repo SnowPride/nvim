9ee02226e4e5bcd4c31e0ad3cb67e757e88b02a9
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-06-10 11:48:32 +0200

    Disable dashboard keymap

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -24,4 +24,4 @@ end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
-vim.keymap.set("n", "<leader>h", "<cmd>Dashboard<cr>", { desc = "Go to homepage" })
+-- vim.keymap.set("n", "<leader>h", "<cmd>Dashboard<cr>", { desc = "Go to homepage" })
